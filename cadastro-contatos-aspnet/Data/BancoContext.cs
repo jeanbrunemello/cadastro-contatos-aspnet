442e55dc59df356dbf6957f701e6c64e48393202
jean <jeanbrunemello@gmail.com>
2024-04-17 22:21:30 -0300

    adição de frontend basico

--- cadastro-contatos-aspnet/Data/BancoContext.cs
+++ cadastro-contatos-aspnet/Data/BancoContext.cs
@@ -1,8 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using cadastro_contatos_aspnet.Models;
 
 namespace cadastro_contatos_aspnet.Data
 {
     public class BancoContext : DbContext
     {
+      public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ContatoModel> Contatos { get; set; }
     }
 }
